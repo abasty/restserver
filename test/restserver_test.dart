892b28e14c92127f16c9edfe2bc51578ce55326f
Alain Basty <alain.basty@free.fr>
2021-04-10 21:45:41 +0200

    Added GET all test

--- test/restserver_test.dart
+++ test/restserver_test.dart
@@ -1,8 +1,22 @@
-import 'package:restserver/restserver.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
+const host = 'localhost:8067';
+
+Future<Map<String, dynamic>> fetchMap(String uri) async {
+  var response = await http.get(Uri.http(host, uri));
+  if (response.statusCode == 200) {
+    return json.decode(response.body) as Map<String, dynamic>;
+  } else {
+    throw Exception('Failed to fetch URI');
+  }
+}
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('GET rayons', () async {
+    var map = await fetchMap('courses/all');
+    print(map);
   });
 }
