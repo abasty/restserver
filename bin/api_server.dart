48d74282021ca6fced6497d4d02c48c6d738f49f
Alain Basty <alain.basty@free.fr>
2021-04-12 14:02:50 +0200

    Code cleaning + deal with sse connections

--- bin/api_server.dart
+++ bin/api_server.dart
@@ -1,9 +1,8 @@
-// import 'dart:convert';
+// @dart=2.9
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:sse/server/sse_handler.dart';
 
 import 'package:restserver/courses_api.dart';
@@ -14,15 +13,20 @@ Future<void> main() async {
   final api = Router();
   api.mount('/courses/', CoursesApi().router);
   final sse = SseHandler(Uri.parse('/sync'));
-  var handler = Cascade().add(sse.handler).add(api).handler;
-
+  var cascade = Cascade().add(sse.handler).add(api).handler;
   // ignore: todo
   // TODO: Add CorsHeaders in Pipeline
-  var server =
-      const Pipeline().addMiddleware(logRequests()).addHandler(handler);
+  var pipeline =
+      const Pipeline().addMiddleware(logRequests()).addHandler(cascade);
 
   print('Launching API server');
-  await io.serve(server, 'localhost', 8067);
+  var server = await io.serve(pipeline, 'localhost', 8067);
+  print('Server launched on ${server.address.address}:${server.port}');
+  var connections = sse.connections;
+  while (await connections.hasNext) {
+    var connection = await connections.next;
+    print('Connection (sse): $connection.');
+  }
 }
 
 // void addCorsHeaders(HttpResponse response) {
